b24a34771f8ba8bdb6e51f9d5861acc11df77c36
CS <azimochilov226@gmail.com>
2023-07-11 14:05:49 +0300

    exception is done

---
+++ InternTask.Service/Exceptions/InternTaskException.cs
@@ -0,0 +1,9 @@
+﻿namespace InternTask.Service.Exceptions;
+public class InternTaskException : Exception
+{
+    public int Code { get; set; }
+    public InternTaskException(int code = 500, string message = "Something went wrong") : base(message)
+    {
+        this.Code = code;
+    }
+}
